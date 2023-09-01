64a3eb4edcd5dbe5211265966b10de56bb172a1b
Zonarius <zonarius@live.at>
2023-09-01 08:13:32 +0200

    Add dummy input files for each day

--- create-folders.sh
+++ create-folders.sh
@@ -2,6 +2,10 @@
 
 for INDEX in {1..25}
 do
-  cargo new --vcs none day$INDEX-1
-  cargo new --vcs none day$INDEX-2
+  for PART in {1..2}
+  do
+    cargo new --vcs none day$INDEX-$PART
+    touch day$INDEX-$PART/test-input.txt
+    touch day$INDEX-$PART/input.txt
+  done
 done
